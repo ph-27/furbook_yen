6273bad3f7ae89aacc97a70b20fcad8ba6ef0ee8
yentyty <nguyenthiyen.tk20@gmail.com>
2018-10-04 18:39:51 +0700

    update day 3 - #1

---
+++ app/Cat.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace furbook;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cat extends Model
+{
+    /**
+     * White list column of cats table
+     */
+    protected $fillable = [
+        'name',
+        'date_of_bith',
+        'breed_id',
+    ];
+
+    public function breed() {
+        return $this->belongsTo('furbook\Breed', 'breed_id', 'id');
+    }
+}
